3b766a07a7e77fa11d3db9a2d8a8c03d379ecfbd
sownthunder <dbates117@gmail.com>
2019-08-19 11:45:44 -0400

    first bojangles my tangles

---
+++ test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int
+	i = 42
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	firstName := "Arthur"
+	fmt.Println(firstName)
+
+	b := true
+	fmt.Println(b)
+
+	c := complex(3, 4)
+	fmt.Println(c)
+
+	r, im := real(c), imag(c)
+	fmt.Println(r, im)
+
+}
